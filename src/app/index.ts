974e325125bfe98adddbde263e62b8f7bc1d698f
l10178 <l10178@163.com>
2018-01-18 22:23:12 +0800

    exports  pipe and component

---
+++ src/app/index.ts
@@ -0,0 +1,1 @@
+export * from './prettysize/pretty-size.module';
--- src/app/prettysize/index.ts
+++ src/app/prettysize/index.ts
@@ -1,1 +1,5 @@
+export * from './pretty-size.component';
 export * from './pretty-size.module';
+export * from './pretty-size.pipe';
+export * from './pretty-size.service';
+export * from './pretty-size-options';
