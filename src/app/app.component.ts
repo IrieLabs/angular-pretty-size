47485dede9cb72604d7e4210d80d75f8ac910cb6
l10178 <l10178@163.com>
2018-03-09 23:33:49 +0800

    update default service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pretty-size';
   smallBytes = [10, 500, 1000, 1024, 1189];
   largeBytes = [100000, 102400, 622468864, 792382830, 892382830];
   hugeBytes = [1000000000, 1024000000, 7224688640, 8923828300, 18923828300000];
